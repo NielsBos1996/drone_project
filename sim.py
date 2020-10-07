dc9a05c91d21fa2ebe3305eef26ade298548cc80
niels <niels.vd.bos@hotmail.com>
2020-10-07 15:20:57 +0200

    [refactored]

--- sim.py
+++ sim.py
@@ -1,17 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import io
+import subprocess
+import functools
 import imageio
-from PIL import Image
+import time
+import io
+import os
 
 
-def scatter(x, y, z, c):
-    n = 100
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    ax.scatter(x, y, z, c=c)
-    return fig
+def timer(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        st = time.time()
+        result = func(*args, **kwargs)
+        print(f'Executing time: {time.time()-st:.4f} seconds')
+        return result
+    return wrapper
 
 
 def test():
@@ -23,25 +27,41 @@ def test():
     images = []
 
     for time in range(100):
-        z[0] = time / 100
-
-        fig = plt.figure()
-        ax = fig.add_subplot(1,1,1, projection='3d')
-        ax.scatter(x, y, z, c)
-        ax.set_zlim(0, 1)
-
-        # fig = scatter(x, y, z, c)
-        # fig.set_zlim(0, 1)
         buf = io.BytesIO()
+        fig = scatter(x, y, time/100, c, zmin=0, zmax=1, title=f'time: {time}')
         fig.savefig(buf, format='png')
         images.append(buf)
         plt.close()
+    make_gif(images, name='test')
 
-    with imageio.get_writer('./test_drone.gif', mode='I') as writer:
+
+def scatter(x, y, z, c, xmin=None, xmax=None, ymin=None, ymax=None, zmin=None,
+            zmax=None, title=""):
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1, projection='3d')
+    ax.scatter(x, y, z, c)
+    if xmin is not None and xmax is not None:
+        ax.set_xlim(xmin, xmax)
+    if ymin is not None and ymax is not None:
+        ax.set_ylim(ymin, ymax)
+    if zmin is not None and zmax is not None:
+        ax.set_zlim(zmin, zmax)
+    if title:
+        ax.set_title(title)
+    return fig
+
+
+def make_gif(images: list, name: str, show=True):
+    if not name.endswith('.gif'):
+        name += '.gif'
+    with imageio.get_writer(name, mode='I') as writer:
         for path_to_image in images:
             path_to_image.seek(0)
             img = imageio.imread(path_to_image)
             writer.append_data(img)
+    if show:
+        subprocess.check_output([f'{os.getcwd()}\\{name}'], shell=True)
+
 
 test()
 
